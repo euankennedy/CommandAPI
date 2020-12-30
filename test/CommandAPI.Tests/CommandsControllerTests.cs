12a1b13c0990ecb11b3b4125332e0c6f0ea79f09
Euan <euan.kennedy@mojosoup.com.au>
2020-12-30 17:56:04 +1000

    fix broken test

--- test/CommandAPI.Tests/CommandsControllerTests.cs
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -202,8 +202,7 @@ namespace CommandAPI.Tests
             //Act
             var result = controller.DeleteCommand(0);
             //Assert
-            //Assert.IsType<NotFoundResult>(result);
-            Assert.IsType<OkResult>(result);
+            Assert.IsType<NotFoundResult>(result);
         }
 
         private List<Command> GetCommands(int num)
